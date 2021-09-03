ae71ececfed0d0306fefe8d0ad7d4239c00f7ae5
nikmihalevich <nikandr33@inbox.ru>
2021-09-03 10:49:00 +0300

    added in standart modules

---
+++ upload/catalog/controller/extension/module/special.php
@@ -0,0 +1,80 @@
+<?php
+class ControllerExtensionModuleSpecial extends Controller {
+	public function index($setting) {
+		$this->load->language('extension/module/special');
+
+		$this->load->model('catalog/product');
+
+		$this->load->model('tool/image');
+
+		$data['products'] = array();
+
+		$filter_data = array(
+			'sort'  => 'pd.name',
+			'order' => 'ASC',
+			'start' => 0,
+			'limit' => $setting['limit']
+		);
+
+		$results = $this->model_catalog_product->getProductSpecials($filter_data);
+
+		if ($results) {
+			foreach ($results as $result) {
+				if ($result['image']) {
+					$image = $this->model_tool_image->resize($result['image'], $setting['width'], $setting['height']);
+				} else {
+					$image = $this->model_tool_image->resize('placeholder.png', $setting['width'], $setting['height']);
+				}
+
+				if ($this->customer->isLogged() || !$this->config->get('config_customer_price')) {
+					$price = $this->currency->format($this->tax->calculate($result['price'], $result['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+				} else {
+					$price = false;
+				}
+
+				if ((float)$result['special']) {
+					$special = $this->currency->format($this->tax->calculate($result['special'], $result['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+				} else {
+					$special = false;
+				}
+
+				if ($this->config->get('config_tax')) {
+					$tax = $this->currency->format((float)$result['special'] ? $result['special'] : $result['price'], $this->session->data['currency']);
+				} else {
+					$tax = false;
+				}
+
+				if ($this->config->get('config_review_status')) {
+					$rating = $result['rating'];
+				} else {
+					$rating = false;
+				}
+
+                $labels = $this->model_catalog_product->getProductLabels($result['product_id']);
+
+                foreach ($labels as $key => $label) {
+                    if ($label['image']) {
+                        $labels[$key]['image'] = $this->model_tool_image->resize($label['image'], 25, 25);
+                    } else {
+                        $labels[$key]['image'] = $this->model_tool_image->resize('placeholder.png', 25, 25);
+                    }
+                }
+
+				$data['products'][] = array(
+					'product_id'  => $result['product_id'],
+					'thumb'       => $image,
+					'name'        => $result['name'],
+					'description' => utf8_substr(trim(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8'))), 0, $this->config->get('theme_' . $this->config->get('config_theme') . '_product_description_length')) . '..',
+					'price'       => $price,
+					'special'     => $special,
+					'tax'         => $tax,
+					'rating'      => $rating,
+                    'labels'      => $labels,
+                    'href'        => $this->url->link('product/product', 'product_id=' . $result['product_id'])
+				);
+			}
+
+			return $this->load->view('extension/module/special', $data);
+		}
+	}
+}
